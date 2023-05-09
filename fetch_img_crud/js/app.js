cca39c32aa2e1d75a10ec2a8d0bed531017425b2
ngonhattung <ngonhattung08062003@gmail.com>
2023-05-09 16:44:33 +0700

    hoan chinh delete va edit

--- fetch_img_crud/js/app.js
+++ fetch_img_crud/js/app.js
@@ -1,13 +1,12 @@
 var linkAPI = 'https://64599b8995624ceb21ebe122.mockapi.io/users';
-var linkAPIIMG = 'https://api.slingacademy.com/v1/sample-data/photos';
+
 var btnCreateUser = document.getElementById('btnCreate');
 var idValue = document.getElementById('idInput');
-var titleValue = document.getElementById('titleInput');
-var bodyValue = document.getElementById('bodyInput');
-
+var nameValue = document.getElementById('titleInput');
+var jobValue = document.getElementById('bodyInput');
+var imgValue = document.getElementById('inputIMG');
 function start() {
     getAPI(render);
-    getAPIIMG(render);
     handleCreateUser()
 }
 start();
@@ -41,13 +40,13 @@ function render(users) {
     users.map(function(user) {
         return table +=
             `
-        <tr>
-            <td>${user.id}</td>
-            <td>${user.name}</td>
-            <td>${user.job}</td>
-            <td><img src="https://d1hjkbq40fs2x4.cloudfront.net/2017-08-21/files/landscape-photography_1645.jpg" alt="" style="width:100px;height: 100px;"></td>
-            <td><button>Delete</button></td>
-            <td><button>Edit</button></td>
+        <tr class ="row-${user.id}">
+            <td class = "id-User-${user.id}">${user.id}</td>
+            <td class = "name-User-${user.id}">${user.name}</td>
+            <td class = "job-User-${user.id}">${user.job}</td>
+            <td><img class = "linkImg-User-${user.id}" src="${user.url}" alt="" style="width:100px;height: 100px;"></td>
+            <td onclick = "handleDeleteUser(${user.id})"><button>Delete</button></td>
+            <td onclick = "handleEditUser(${user.id})"><button>Edit</button></td>
         </tr>
         `;
     })
@@ -73,11 +72,89 @@ function handleCreateUser() {
     btnCreateUser.onclick = function() {
         var obj = {
             id: idValue.value,
-            name: titleValue.value,
-            job: bodyValue.value
+            name: nameValue.value,
+            job: jobValue.value,
+            url: imgValue.value
         };
         createUserAPI(obj, function() {
             getAPI(render);
         });
     }
+}
+function handleDeleteUser(id)
+{
+    fetch(linkAPI + '/' + id, {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json"
+        },
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(){
+        var rowItem = document.querySelector('.row-'+id);
+        if(rowItem)
+        {
+            rowItem.remove();
+        }
+    });
+}
+function editUserAPI(data,callback,id)
+{
+    fetch(linkAPI + '/' + id, {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(callback);
+}
+function handleEditUser(id)
+{
+    var idUser = document.querySelector('.id-User-'+id).textContent;
+    var nameUser = document.querySelector('.name-User-'+id).textContent;
+    var jobUser = document.querySelector('.job-User-'+id).textContent;
+    var linkIMGUser = document.querySelector('.linkImg-User-'+id).src;
+    
+    idValue.value = idUser;
+    nameValue.value = nameUser;
+    jobValue.value = jobUser;
+    imgValue.value = linkIMGUser;
+
+
+    btnCreateUser.innerText = 'Save'
+
+    if( btnCreateUser.innerText == 'Save')
+    {
+        btnCreateUser.onclick = function()
+    {
+        var data = {
+            id:idValue.value,
+            name:nameValue.value,
+            job:jobValue.value,
+            url:imgValue.value
+        }
+
+        editUserAPI(data,function()
+        {
+            getAPI(render);
+        },id)
+        
+        clear();
+    }
+    }
+}
+
+function clear()
+{
+    btnCreateUser.innerText = 'Create User'
+    idValue.value = '';
+    nameValue.value = '';
+    jobValue.value = '';
+    imgValue.value = '';
 }
